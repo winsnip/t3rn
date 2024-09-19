f59a2cd117591e17e129aa8a987e1644b290b6f5
WINSNIP <winsnipoffcial@gmail.com>
2024-09-20 01:19:33 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-apt install python3-pip python3 nano -y && pip install -r linux.txt && nano .env && python main.py
+apt install python3-pip python3 nano -y && pip install -r linux.txt && nano .env && echo "python main.py" > main.sh && python main.py
